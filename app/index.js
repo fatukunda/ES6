0b8d99afda2df3d1b1f30de7b389c0483f1a96b5
Frank <fatukunda@gmail.com>
2018-07-26 18:00:36 +0300

    declaring variables with const

--- app/index.js
+++ app/index.js
@@ -1,12 +1,19 @@
-function run() {
-    //variable b declared
-    let b = 2;
-    console.log(b);
-    //Another variable b declared in a different scope and doesn't affect the other variable b above
-    {
-        let b = 3;
-        console.log(b);
-    }
+//Diclaring variables with let
+// function run() {
+//     //variable b declared
+//     let b = 2;
+//     console.log(b);
+//     //Another variable b declared in a different scope and doesn't affect the other variable b above
+//     {
+//         let b = 3;
+//         console.log(b);
+//     }
 
-}
-run();
+// }
+// run();
+
+//Diclaring variables with const
+const numbers = [1,2,3];
+//the variable cannot be re-assigned but it can be modified.
+numbers.push(4)
+console.log(numbers);
